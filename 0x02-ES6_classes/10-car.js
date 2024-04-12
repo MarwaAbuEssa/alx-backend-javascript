eb022b89068355278ea497102e4bbbad25be7a77
MarwaAbuEssa <meromeroo@gmail.com>
2024-04-13 01:05:21 +0200

    fixing 10 car

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -3,12 +3,12 @@ export default class Car {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
-}
-  
+  }
+
   static get [Symbol.species]() {
     return this.prototype.constructor;
   }
-  
+ 
   cloneCar() {
     return new (this.constructor[Symbol.species])();
   }
